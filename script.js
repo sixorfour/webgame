10739d50c157b51510d4885f3e0f8662cf909482
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-06-29 22:04:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,94 +6,97 @@ let lastEnergyUpdate = new Date().getTime();
 let resultTimeout;
 
 function updateResources() {
-    document.getElementById('wood').textContent = `Wood: ${wood}`;
-    document.getElementById('stone').textContent = `Stone: ${stone}`;
-    document.getElementById('food').textContent = `Food: ${food}`;
-    document.getElementById('energy').textContent = `Energy: ${energy}`;
+  document.getElementById('wood').textContent = `Wood: ${wood}`;
+  document.getElementById('stone').textContent = `Stone: ${stone}`;
+  document.getElementById('food').textContent = `Food: ${food}`;
+  document.getElementById('energy').textContent = `Energy: ${energy}`;
 }
 
 function chopWood() {
-    if (energy > 0) {
-        energy--;
-        const amount = getRandomAmount();
-        wood += amount;
-        updateResources();
-        displayResult(`Chopped ${amount} wood.`);
-    } else {
-        displayEnergyMessage();
-    }
+  if (energy > 0) {
+    energy--;
+    const amount = getRandomAmount();
+    wood += amount;
+    updateResources();
+    displayResult(`Chopped ${amount} wood.`);
+  } else {
+    displayEnergyMessage();
+  }
 }
 
 function gatherStone() {
-    if (energy > 0) {
-        energy--;
-        const amount = getRandomAmount();
-        stone += amount;
-        updateResources();
-        displayResult(`Gathered ${amount} stone.`);
-    } else {
-        displayEnergyMessage();
-    }
+  if (energy > 0) {
+    energy--;
+    const amount = getRandomAmount();
+    stone += amount;
+    updateResources();
+    displayResult(`Gathered ${amount} stone.`);
+  } else {
+    displayEnergyMessage();
+  }
 }
 
 function forageFood() {
-    if (energy > 0) {
-        energy--;
-        const amount = getRandomAmount();
-        food += amount;
-        updateResources();
-        displayResult(`Foraged ${amount} food.`);
-    } else {
-        displayEnergyMessage();
-    }
+  if (energy > 0) {
+    energy--;
+    const amount = getRandomAmount();
+    food += amount;
+    updateResources();
+    displayResult(`Foraged ${amount} food.`);
+  } else {
+    displayEnergyMessage();
+  }
 }
 
 function getRandomAmount() {
-    return Math.floor(Math.random() * 50) + 1;
+  return Math.floor(Math.random() * 50) + 1;
 }
 
 function displayResult(message) {
-    const resultContainer = document.getElementById('result');
-    clearTimeout(resultTimeout); // Clear any existing timeouts
-    resultContainer.textContent = message;
-    resultTimeout = setTimeout(() => {
-        resultContainer.textContent = '';
-    }, 3000); // Display for 3 seconds
+  const resultContainer = document.getElementById('result');
+  clearTimeout(resultTimeout); // Clear any existing timeouts
+  resultContainer.textContent = message;
+  resultTimeout = setTimeout(() => {
+    resultContainer.textContent = '';
+  }, 3000); // Display for 3 seconds
 }
 
 function displayEnergyMessage() {
-    const currentTime = new Date().getTime();
-    const elapsedSeconds = Math.floor((currentTime - lastEnergyUpdate) / 1000);
-    const remainingSeconds = 60 - (elapsedSeconds % 60);
-    const remainingTime = new Date(remainingSeconds * 1000).toISOString().substr(14, 5);
+  const currentTime = new Date().getTime();
+  const elapsedSeconds = Math.floor((currentTime - lastEnergyUpdate) / 1000);
+  const remainingSeconds = 60 - (elapsedSeconds % 60);
+  const remainingTime = new Date(remainingSeconds * 1000).toISOString().substr(14, 5);
 
-    const message = `You don't have enough energy! Come back in ${remainingTime} for more energy.`;
-    alert(message);
+  const message = `You don't have enough energy! Come back in ${remainingTime} for more energy.`;
+  alert(message);
 }
 
 function regenerateEnergy() {
-    const currentTime = new Date().getTime();
-    const elapsedSeconds = Math.floor((currentTime - lastEnergyUpdate) / 1000);
-    const energyToRegenerate = Math.floor(elapsedSeconds / 60);
+  const currentTime = new Date().getTime();
+  const elapsedMilliseconds = currentTime - lastEnergyUpdate;
+  const energyToRegenerate = Math.floor(elapsedMilliseconds / (60 * 1000)); // Convert milliseconds to minutes
 
-    if (energyToRegenerate > 0) {
-        energy = Math.min(10, energy + energyToRegenerate);
-        lastEnergyUpdate = currentTime;
-        updateResources();
-    }
+  if (energyToRegenerate > 0) {
+    energy = Math.min(3, energy + energyToRegenerate);
+    lastEnergyUpdate = currentTime;
+    updateResources();
+  }
+
+  // Schedule the next energy regeneration
+  setTimeout(regenerateEnergy, 60 * 1000); // Regenerate every 1 minute
 }
 
 function resetGame() {
-    energy = 3;
-    wood = 0;
-    stone = 0;
-    food = 0;
-    lastEnergyUpdate = new Date().getTime();
-    updateResources();
+  energy = 3;
+  wood = 0;
+  stone = 0;
+  food = 0;
+  lastEnergyUpdate = new Date().getTime();
+  updateResources();
 }
 
 // Regenerate energy every minute
-setInterval(regenerateEnergy, 60000); // 60,000 milliseconds = 1 minute
+setTimeout(regenerateEnergy, 60 * 1000); // Initial energy regeneration after 1 minute
 
 // Initial update of resources display
 updateResources();
